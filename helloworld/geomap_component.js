a221b8fef46dedd6546c5412ebaccf99cf23ec35
aclaessen <121887238+aclaessen@users.noreply.github.com>
2023-11-30 13:16:02 +0100

    Update geomap_component.js

--- helloworld/geomap_component.js
+++ helloworld/geomap_component.js
@@ -126,7 +126,7 @@
                 // replace the ID below with the ID to your web map
                 const webmap = new WebMap ({
                     portalItem: {
-                        id: "a17e134c51f74252bca8db3c66ef032e"
+                        id: "27da9e3c5ded490c9b8f84963a39baf5" //"a17e134c51f74252bca8db3c66ef032e"
                     }
                 });
 
