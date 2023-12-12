327b1f1cf1fc7eceade6610a1e73552e196bd3b8
aclaessen <121887238+aclaessen@users.noreply.github.com>
2023-12-12 10:14:28 +0100

    Update geomap_component.js

--- SAC_integration/geomap_component.js
+++ SAC_integration/geomap_component.js
@@ -1,8 +1,8 @@
 (function() {
     let template = document.createElement("template");
     var gPassedServiceType; // holds passed in guarantee of service - set in onCustomWidgetBeforeUpdate()
-    var gPassedPortalURL; // ESRI Portal URL
-    var gPassedAPIkey; // ESRI JS api key
+    var gPassedPortalURL; //ESRI Portal URL
+    var gPassedAPIkey; //ESRI JS api key
     var gWebmapInstantiated = 0; // a global used in applying definition query
     var gMyLyr; // for sublayer
     var gMyWebmap; // needs to be global for async call to onCustomWidgetAfterUpdate()
@@ -14,25 +14,19 @@
             width: 100%;
             height: 100%;
         }
-        #timeSlider {
-            position: absolute;
-            left: 5%;
-            right: 15%;
-            bottom: 20px;
-        }
         </style>
         <div id='mapview'></div>
         <div id='timeSlider'></div>
     `;
-
+    
     // this function takes the passed in servicelevel and issues a definition query
     // to filter service location geometries
     //
     // A definition query filters what was first retrieved from the SPL feature service
     function applyDefinitionQuery() {
-        var svcLyr = gMyWebmap.findLayerById('0');
-        console.log("Layer is");
-        console.log(svcLyr);
+        var svcLyr = this.gMyWebmap.findLayerById( '185bcfada7844c5ab9182e0e8b5845e0' ); 
+        console.log( "Layer is");
+        console.log( svcLyr);
 
         // make layers visible
         svcLyr.visible = true;
@@ -40,10 +34,10 @@
         // only execute when the sublayer is loaded. Note this is asynchronous
         // so it may be skipped over during execution and be executed after exiting this function
         svcLyr.when(function() {
-            gMyLyr = svcLyr.findSublayerById(0);    // store in global variable
+            gMyLyr = svcLyr.findSublayerById(6);    // store in global variable
             console.log("Sublayer loaded...");
-            console.log("Sublayer is");
-            console.log(gMyLyr);
+            console.log( "Sublayer is");
+            console.log( gMyLyr);
 
             // force sublayer visible
             gMyLyr.visible = true;
@@ -54,7 +48,8 @@
     };
 
     // process the definition query on the passed in SPL feature sublayer
-    function processDefinitionQuery() {
+    function processDefinitionQuery()
+    {
         // values of passedServiceType
         // 0, 1 - no service levels. Only show service locations without a guarantee of service (GoS)
         //     Note that 0 is passed in when the widget is initialized and 1 on subsequent times
@@ -101,19 +96,29 @@
                 "esri/layers/FeatureLayer",
                 "esri/widgets/TimeSlider",
                 "esri/widgets/Expand",
+                "esri/tasks/RouteTask",
+                "esri/tasks/support/RouteParameters",
+                "esri/tasks/support/FeatureSet",
+                "esri/tasks/support/Query",
+                "esri/layers/support/Sublayer",
                 "esri/Graphic",
                 "esri/views/ui/UI",
                 "esri/views/ui/DefaultUI" 
-            ], function(esriConfig, WebMap, MapView, BasemapToggle, FeatureLayer, TimeSlider, Expand, Graphic) {
+            ], function(esriConfig, WebMap, MapView, BasemapToggle, FeatureLayer, TimeSlider, Expand, RouteTask, RouteParameters, FeatureSet, Query, Sublayer, Graphic) {
         
                 // set portal and API Key
                 esriConfig.portalUrl = gPassedPortalURL
 
                 //  set esri api Key 
-                esriConfig.apiKey = gPassedAPIkey;
-
+                esriConfig.apiKey = gPassedAPIkey
+        
+                // set routing service
+                var routeTask = new RouteTask({
+                    url: ""
+                });
+        
                 // replace the ID below with the ID to your web map
-                const webmap = new WebMap({
+                const webmap = new WebMap ({
                     portalItem: {
                         id: "eedbcdc058d9466d989487747d984ea5" //"a17e134c51f74252bca8db3c66ef032e"
                     }
@@ -127,14 +132,14 @@
                 });
 
                 // time slider widget initialization
-                const timeSlider = new TimeSlider({
-                    container: "timeSlider",
-                    view: view
-                });
-
+                //const timeSlider = new TimeSlider({
+                //    container: "timeSlider",
+                //    view: view
+                //});
+        
                 // set on click for directions
                 view.on("click", addStop);
-
+        
                 function addGraphic(type, point) {
                     var graphic = new Graphic({
                         symbol: {
@@ -148,11 +153,161 @@
                     view.graphics.add(graphic);
                 }
 
-                function addStop(event) {
-                    // no code here
+                function addStop( event) { // no code here
                     // here neither
                     if (view.graphics.length === 0) {
                         addGraphic("start", event.mapPoint);
                     } else if (view.graphics.length === 1) {
-                        addGraphic("finish", event.map
+                        addGraphic("finish", event.mapPoint);
+                        getRoute();
+                    } else {
+                        view.graphics.removeAll();
+                        addGraphic("start", event.mapPoint);
+                    }
+                };
 
+                function getRoute() {
+                    // Setup the route parameters
+                    var routeParams = new RouteParameters({
+                        stops: new FeatureSet({
+                            features: view.graphics.toArray() // Pass the array of graphics
+                        }),
+                        returnDirections: true
+                    });
+
+                    // Get the route
+                    routeTask.solve(routeParams).then( showRoute);
+                }
+
+                function showRoute( data)
+                {
+                    // Display the route
+                    
+                    data.routeResults.forEach(function (result) {
+                        result.route.symbol = {
+                            type: "simple-line",
+                            color: [5, 150, 255],
+                            width: 3
+                        };
+                        view.graphics.add(result.route);
+                    });
+
+                    // Display the directions
+                    var directions = document.createElement("ol");
+                    directions.classList = "esri-widget esri-widget--panel esri-directions__scroller";
+                    directions.style.marginTop = 0;
+                    directions.style.paddingTop = "15px";
+        
+                    // Show the directions
+                    var features = data.routeResults[0].directions.features;
+                    features.forEach(function (result, i) {
+                        var direction = document.createElement("li");
+                        direction.innerHTML =
+                        result.attributes.text + " (" + result.attributes.length.toFixed(2) + " miles)";
+                        directions.appendChild(direction);
+                    });
+
+                    // Add directions to the view
+                    view.ui.empty("top-right");
+                    view.ui.add(directions, "top-right");
+                }
+
+                view.when(function () {
+                    view.popup.autoOpenEnabled = true; //disable popups
+                    gWebmapInstantiated = 1; // used in onCustomWidgetAfterUpdate
+        
+                    // Create the basemap toggle
+                    var basemapToggle = new BasemapToggle({
+                        view:view,
+                        nextBasemap: "satellite"
+                    });
+
+        
+                    // Add the toggle to the bottom-right of the view
+                    view.ui.add( basemapToggle, "bottom-right");
+        
+                    // should have been set in onCustomWidgetBeforeUpdate()
+                    console.log( gPassedServiceType);
+
+                    // find the SPL sublayer so a query is issued
+                    applyDefinitionQuery();
+                });
+
+            }); // end of require()
+        } // end of constructor()    
+
+        getSelection() {
+            return this._currentSelection;
+        }
+
+        onCustomWidgetBeforeUpdate(changedProperties)
+        {
+            this._props = { ...this._props, ...changedProperties };
+            console.log(["Service Level",changedProperties["servicelevel"]]);
+
+        }
+
+        onCustomWidgetAfterUpdate(changedProperties) 
+        {
+            if ("servicelevel" in changedProperties) {
+                this.$servicelevel = changedProperties["servicelevel"];
+            }
+            gPassedServiceType = this.$servicelevel; // place passed in value into global
+
+            if ("portalurl" in changedProperties) {
+                this.$portalurl = changedProperties["portalurl"];
+            }
+            gPassedPortalURL = this.$portalurl; // place passed in value into global
+
+            if ("apikey" in changedProperties) {
+                this.$apikey = changedProperties["apikey"];
+            }
+            gPassedAPIkey = this.$apikey; // place passed in value into global
+
+            // only attempt to filter displayed service locations if the webmap is initialized
+           if (gWebmapInstantiated === 1) {
+                applyDefinitionQuery();
+            }
+        }
+    } // end of class
+
+
+
+
+    let scriptSrc = "https://js.arcgis.com/4.18/"
+    let onScriptLoaded = function() {
+        customElements.define("com-sap-custom-geomap", Map);
+    }
+
+    //SHARED FUNCTION: reuse between widgets
+    //function(src, callback) {
+    let customElementScripts = window.sessionStorage.getItem("customElementScripts") || [];
+    let scriptStatus = customElementScripts.find(function(element) {
+        return element.src == scriptSrc;
+    });
+
+    if (scriptStatus) {
+        if(scriptStatus.status == "ready") {
+            onScriptLoaded();
+        } else {
+            scriptStatus.callbacks.push(onScriptLoaded);
+        }
+    } else {
+        let scriptObject = {
+            "src": scriptSrc,
+            "status": "loading",
+            "callbacks": [onScriptLoaded]
+        }
+        customElementScripts.push(scriptObject);
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = scriptSrc;
+        script.onload = function(){
+            scriptObject.status = "ready";
+            scriptObject.callbacks.forEach((callbackFn) => callbackFn.call());
+        };
+        document.head.appendChild(script);
+    }
+
+//END SHARED FUNCTION
+})(); // end of class
